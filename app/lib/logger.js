8c1e6ba8a2ae3a640e64877f6427013a90b22a19
Martin Gamnitzer <kontakt@martin-gamnitzer.de>
2016-11-28 22:18:02 +0100

    always log to stdout, for docker

--- app/lib/logger.js
+++ app/lib/logger.js
@@ -8,13 +8,17 @@ const common = require('./common');
 
 // in dev, only log to stdout, in any other case log to filename
 let transports = [];
-switch(common.getEnv()) {
+/*switch(common.getEnv()) {
   case 'development':
     transports.push(new (winston.transports.Console)({ level: 'debug', colorize: true, prettyPrint: true }));
     break;
   default:
     transports.push(new (winston.transports.File)({ filename: common.getBasePath()+'/log/app.log', level: 'info' }));
 }
+*/
+
+// always log to stdout
+transports.push(new (winston.transports.Console)({ level: 'debug', colorize: true, prettyPrint: true }));
 
 const logger = new (winston.Logger)({
   transports: transports
